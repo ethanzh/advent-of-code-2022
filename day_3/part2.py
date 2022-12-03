bbc93f06edf4568357c07056765b6fbdec8101cc
Ethan Houston <ethan.houston@gmail.com>
2022-12-03 11:05:13 -0800

    day 3: format one-liner to be slightly readable

--- day_3/part2.py
+++ day_3/part2.py
@@ -1,5 +1,17 @@
 from functools import reduce
 from more_itertools import batched
 
-print(sum([(lambda x: ord(x) - (38 if x.isupper() else 96))(reduce(lambda x, y: x & y, [set(c for c in items) for items in group]).pop()) for group in batched([line.strip() for line in open("input").readlines()], 3)]))
-
+print(
+    sum(
+        [
+            (lambda x: ord(x) - (38 if x.isupper() else 96))(
+                reduce(
+                    lambda x, y: x & y, [set(c for c in items) for items in group]
+                ).pop()
+            )
+            for group in batched(
+                [line.strip() for line in open("input").readlines()], 3
+            )
+        ]
+    )
+)
