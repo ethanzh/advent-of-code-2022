7b420580ccc268c3c53ac4abe4667c36273ee78d
Ethan Houston <ethan.houston@gmail.com>
2022-12-03 11:03:32 -0800

    day 3: refactor into one-liner

--- day_3/part2.py
+++ day_3/part2.py
@@ -1,22 +1,5 @@
 from functools import reduce
+from more_itertools import batched
 
-with open("input") as f:
-	lines = [line.strip() for line in f.readlines()]
+print(sum([(lambda x: ord(x) - (38 if x.isupper() else 96))(reduce(lambda x, y: x & y, [set(c for c in items) for items in group]).pop()) for group in batched([line.strip() for line in open("input").readlines()], 3)]))
 
-priority_sum = 0
-
-n = 3
-groups = [lines[i: i + n] for i in range(0, len(lines), n)]
-
-total_score = 0
-
-for group in groups:
-	sets = [set(c for c in items) for items in group]
-	intersection = reduce(lambda x, y: x & y, sets).pop()
-
-	if intersection.isupper():
-		total_score += ord(intersection) - 38
-	else:
-		total_score += ord(intersection) - 96
-
-print(total_score)
